fbaccdadce411f9015bda35cc0828a4f1f520caa
Jin <jh@vitamate.kr>
2023-01-22 05:19:05 +0900

    change api server domain

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
-      console.log("Server has started on port http://localhost:8080")
+      console.log("Server has started on port https://dall-e-nj6d.onrender.com")
     );
   } catch (error) {
     console.log(error);
